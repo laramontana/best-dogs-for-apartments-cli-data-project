c77d2000ddfde7b0bb5b062fa8f2e4a012ee8c1c
Lara Matyushenko <larabrightside@gmail.com>
2017-06-08 19:42:29 +0000

    Initial file setup

---
+++ lib/cli-controller.rb
@@ -0,0 +1,12 @@
+require_relative "../lib/scraper.rb"
+require_relative "../lib/dog.rb"
+require 'nokogiri'
+require 'colorize'
+
+class CLIController
+
+  def run
+
+  end
+
+end
---
+++ lib/scraper.rb
@@ -0,0 +1,7 @@
+require 'open-uri'
+require 'nokogiri'
+require 'pry'
+
+class Scraper
+
+end
