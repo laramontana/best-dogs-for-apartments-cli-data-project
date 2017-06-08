1d91ef51aec7018c72a766f4be867dccc359ad7b
Lara Matyushenko <larabrightside@gmail.com>
2017-06-08 20:25:19 +0000

    Add #self.scrape_main_page in Scraper class (don't know if it's working yet)

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,5 +3,21 @@ require 'nokogiri'
 require 'pry'
 
 class Scraper
+  # "http://www.akc.org/dog-breeds/best-dogs-for-apartments/")
+  # http://www.akc.org/dog-breeds/best-dogs-for-apartments/)
+
+
+  def self.scrape_main_page(main_page)
+    doc = Nokogiri::HTML(open("#{main_page}"))
+    doc.css(".event.scale-img-parent").collect do |dog_card|
+      {
+        :breed => dog_card.css("h2 a").text
+        :description => dog_card.css("h2 p").text
+        :breed_url => dog_card.css("h2 a").attr("href").value
+      }
+
+    end
+  end
+
 
 end
