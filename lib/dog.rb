73e4e6d4a23eae5dd7b90f03356cb77dd164cb19
Lara Matyushenko <larabrightside@gmail.com>
2017-06-12 18:24:00 +0000

    Dog class - #initialize fixed, #self.all added

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,8 +1,15 @@
 class Dog
   attr_accessor :breed, :description, :breed_url, :nutrition_and_feeding, :coat_and_grooming, :exercise, :health
 
+  @@all = []
+
   def initialize(dog_main_info)
     dog_main_info.each {|k,v| self.send("#{k}=", v)}
+    @@all << self
+  end
+
+  def self.all
+    @@all
   end
 
 end
