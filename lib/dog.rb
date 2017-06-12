5afcda1459d94552b7bae603b354adbdd7efc929
Lara Matyushenko <larabrightside@gmail.com>
2017-06-12 18:51:10 +0000

    Dog class - #self.create_dogs_with_main_info done

--- lib/dog.rb
+++ lib/dog.rb
@@ -12,9 +12,27 @@ class Dog
     @@all
   end
 
-end
-
 # dog_main_info
 # Dog.new({:breed=>"Bulldog",
 #   :description=>"Calm, courageous, and friendly; dignified but amusing",
 #   :breed_url=>"/dog-breeds/bulldog/"})
+
+  def self.create_dogs_with_main_info(dogs_main_info)
+    dogs_main_info.each do |dog_main_info|
+      new_dog = self.new(dog_main_info)
+    end
+  end
+
+
+  # dogs_main_info
+  # Dog.create_with_main_info([{:breed=>"Bulldog",
+  #   :description=>"Calm, courageous, and friendly; dignified but amusing",
+  #   :breed_url=>"/dog-breeds/bulldog/"},
+  #  {:breed=>"Shih Tzu",
+  #   :description=>"Outgoing, affectionate, playful and charming",
+  #   :breed_url=>"/dog-breeds/shih-tzu/"},
+  #  {:breed=>"Cavalier King Charles Spaniel",
+  #   :description=>"Affectionate, graceful, and gentle",
+  #   :breed_url=>"/dog-breeds/cavalier-king-charles-spaniel/"}])
+
+end
