8564b52cf1e4a0cec1a9dedcbdc4ec66b23fb4d3
Lara Matyushenko <larabrightside@gmail.com>
2017-06-12 14:29:53 +0000

    Scraper class - #self.scrape_main_page method done, but selectors must be changed

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,12 +7,13 @@ class Scraper
   # http://www.akc.org/dog-breeds/best-dogs-for-apartments/)
 
 
-  def self.scrape_main_page(main_page)
-    doc = Nokogiri::HTML(open("#{main_page}"))
+  def self.scrape_main_page(main_page_url)
+    doc = Nokogiri::HTML(open("#{main_page_url}"))
     doc.css(".event.scale-img-parent").collect do |dog_card|
+      binding.pry
       {
-        :breed => dog_card.css("h2 a").text
-        :description => dog_card.css("h2 p").text
+        :breed => dog_card.css("h2 a").text,
+        :description => dog_card.css("h2 p").text,
         :breed_url => dog_card.css("h2 a").attr("href").value
       }
 
