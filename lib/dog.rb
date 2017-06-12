bc5a7840da41147fa2535f1dad55a3d3a54d5848
Lara Matyushenko <larabrightside@gmail.com>
2017-06-12 18:14:50 +0000

    Dog class - #initialize method done; attr_accessors added

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,13 @@
 class Dog
+  attr_accessor :breed, :description, :breed_url, :nutrition_and_feeding, :coat_and_grooming, :exercise, :health
+
+  def initialize(dog_main_info)
+    dog_main_info.each {|k,v| self.send("#{k}=", v)}
+  end
 
 end
+
+# dog_main_info
+# Dog.new({:breed=>"Bulldog",
+#   :description=>"Calm, courageous, and friendly; dignified but amusing",
+#   :breed_url=>"/dog-breeds/bulldog/"})
