c9232468da1b6c14b10f390b18085000a7e943ec
Lara Matyushenko <larabrightside@gmail.com>
2017-06-12 18:00:09 +0000

    Scraper class - #self.scrape_main_page method fixed; Rake console added

--- Gemfile
+++ Gemfile
@@ -1,5 +1,8 @@
 # A sample Gemfile
 source "https://rubygems.org"
 
+gem 'colorize'
 gem 'pry'
 gem 'nokogiri'
+gem 'require_all'
+gem 'rake'
--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,32 +3,31 @@ require 'nokogiri'
 require 'pry'
 
 class Scraper
-  # "http://www.akc.org/dog-breeds/best-dogs-for-apartments/")
-  # http://www.akc.org/dog-breeds/best-dogs-for-apartments/)
 
-
+# Scraper.scrape_main_page("http://www.akc.org/dog-breeds/best-dogs-for-apartments/")
   def self.scrape_main_page(main_page_url)
     doc = Nokogiri::HTML(open("#{main_page_url}"))
-    doc.css(".event.scale-img-parent").collect do |dog_card|
-      binding.pry
+    doc.css("div .scale-contents").collect do |dog_card|
       {
         :breed => dog_card.css("h2 a").text,
-        :description => dog_card.css("h2 p").text,
-        :breed_url => dog_card.css("h2 a").attr("href").value
+        :description => dog_card.css("p").text.gsub(".", ""),
+        :breed_url => dog_card.css("a").attr("href").value
       }
 
     end
   end
+
+  # Scraper.scrape_breed_page("http://www.akc.org/dog-breeds/bulldog/")
 
   def self.scrape_breed_page(breed_page_url)
     doc = Nokogiri::HTML(open("#{breed_page_url}"))
     new_info = {}
     doc.css("div .care-item.slide").each do |info_slide|
       heading = info_slide.css("h3").text
-      new_info[:nutrition_and_feeding] = info_slide.css("p").text if heading.include?("Feeding")
-      new_info[:coat_and_grooming] = info_slide.css("p").text if heading.include?("Coat")
-      new_info[:exercise] = info_slide.css("p").text if heading.include?("Exercise")
-      new_info[:health] = info_slide.css("p").text if heading.include?("Health")
+      new_info[:nutrition_and_feeding] = info_slide.css("p").text.gsub("Read More", "") if heading.include?("Feeding")
+      new_info[:coat_and_grooming] = info_slide.css("p").text.gsub("Read More", "") if heading.include?("Coat")
+      new_info[:exercise] = info_slide.css("p").text.gsub("Read More", "") if heading.include?("Exercise")
+      new_info[:health] = info_slide.css("p").text.gsub("Read More", "") if heading.include?("Health")
     end
     new_info
   end
